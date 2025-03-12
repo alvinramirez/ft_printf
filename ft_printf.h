7201d23615d1330a2789bcdd240bbcb7416a8919
Alvin Ramirez <alvin.ramirez48@gmail.com>
2025-03-12 22:42:46 +0100

    chore: add pointer printing functions prototypes

--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: alvinram <alvinram@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/26 20:48:46 by alvinram          #+#    #+#             */
-/*   Updated: 2025/03/10 21:50:41 by alvinram         ###   ########.fr       */
+/*   Updated: 2025/03/12 22:37:23 by alvinram         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,12 @@
 # include <stdarg.h>
 # include <unistd.h>
 
-int	ft_printf(const char *format, ...);
-int	ft_print_string(char *string);
-int	ft_print_number(int number);
-int	ft_print_unsigned(unsigned int number);
+int			ft_printf(const char *format, ...);
+int			ft_print_string(char *string);
+int			ft_print_number(int number);
+int			ft_print_unsigned(unsigned int number);
+int			ft_print_pointer(unsigned long long pointer);
+static int	ft_pointer_length(unsigned long long pointer);
+static void	ft_search_pointer(unsigned long long pointer);
 
 #endif
