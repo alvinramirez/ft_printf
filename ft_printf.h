1abcda6117df69d14e7190b7e0f2d525dc2bf8e1
Alvin Ramirez <alvin.ramirez48@gmail.com>
2025-03-15 22:34:40 +0100

    fix: update ft_printf.h headers with new function declarations

--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: alvinram <alvinram@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/26 20:48:46 by alvinram          #+#    #+#             */
-/*   Updated: 2025/03/12 22:37:23 by alvinram         ###   ########.fr       */
+/*   Updated: 2025/03/15 22:29:16 by alvinram         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,13 @@
 # include <stdarg.h>
 # include <unistd.h>
 
-int			ft_printf(const char *format, ...);
-int			ft_print_string(char *string);
-int			ft_print_number(int number);
-int			ft_print_unsigned(unsigned int number);
-int			ft_print_pointer(unsigned long long pointer);
-static int	ft_pointer_length(unsigned long long pointer);
-static void	ft_search_pointer(unsigned long long pointer);
+int	ft_printf(const char *format, ...);
+
+int	ft_print_character(char character);
+int	ft_print_string(char *string);
+int	ft_print_number(int number);
+int	ft_print_unsigned(unsigned int number);
+int	ft_print_pointer(unsigned long long pointer);
+int	ft_print_hexadecimal(unsigned int number, const char caseType);
 
 #endif
