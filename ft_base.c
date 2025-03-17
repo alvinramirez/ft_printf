8d67dc0a916ce7702aa1c13c4982618a4667c784
Alvin Ramirez <alvin.ramirez48@gmail.com>
2025-03-17 22:41:27 +0100

    feat: add ft_safe_write in ft_print.h and ft_base.c for safe write handling

---
+++ ft_base.c
@@ -0,0 +1,97 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_basics.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alvinram <alvinram@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/26 20:49:00 by alvinram          #+#    #+#             */
+/*   Updated: 2025/03/17 22:27:14 by alvinram         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+int	ft_safe_write(int fd, const void *buffer, size_t count);
+
+int	ft_print_character(char character)
+{
+	return (ft_safe_write(1, &character, 1));
+}
+
+int	ft_print_string(char *string)
+{
+	int	len;
+	int	result;
+
+	if (!string)
+		string = "(null)";
+	len = 0;
+	while (*string)
+	{
+		result = ft_safe_write(1, string++, 1);
+		if (result < 0)
+			return (-1);
+		len += result;
+	}
+	return (len);
+}
+
+int	ft_print_number(int number)
+{
+	int	len;
+	int	result;
+
+	len = 0;
+	if (number == -2147483648)
+		return (ft_safe_write(1, "-2147483648", 11));
+	if (number < 0)
+	{
+		result = ft_print_character('-');
+		if (result < 0)
+			return (-1);
+		len += result;
+		number = -number;
+	}
+	if (number >= 10)
+	{
+		result = ft_print_number(number / 10);
+		if (result < 0)
+			return (-1);
+		len += result;
+	}
+	result = ft_print_character((number % 10) + '0');
+	if (result < 0)
+		return (-1);
+	return (len += result);
+}
+
+int	ft_print_unsigned(unsigned int number)
+{
+	int	len;
+	int	result;
+
+	len = 0;
+	if (number >= 10)
+	{
+		result = ft_print_unsigned(number / 10);
+		if (result < 0)
+			return (-1);
+		len = result;
+	}
+	result = ft_print_character((number % 10) + '0');
+	if (result < 0)
+		return (-1);
+	len += result;
+	return (len);
+}
+
+int	ft_safe_write(int fd, const void *buffer, size_t count)
+{
+	int	bytes_written;
+
+	bytes_written = write(fd, buffer, count);
+	if (bytes_written < 0)
+		return (-1);
+	return (bytes_written);
+}
--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: alvinram <alvinram@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/26 20:48:46 by alvinram          #+#    #+#             */
-/*   Updated: 2025/03/15 22:29:16 by alvinram         ###   ########.fr       */
+/*   Updated: 2025/03/17 22:05:26 by alvinram         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,5 +24,7 @@ int	ft_print_number(int number);
 int	ft_print_unsigned(unsigned int number);
 int	ft_print_pointer(unsigned long long pointer);
 int	ft_print_hexadecimal(unsigned int number, const char caseType);
+
+int ft_safe_write(int fd, const void *buffer, size_t count);
 
 #endif
