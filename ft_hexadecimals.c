e5fc9af595ff7260d3ae42ac427f9f503e640b5f
Alvin Ramirez <alvin.ramirez48@gmail.com>
2025-03-24 23:28:10 +0100

    refactor: change caseType from char to int in ft_print_hexadecimal for clarify and to avoid errors

--- ft_hexadecimals.c
+++ ft_hexadecimals.c
@@ -6,13 +6,13 @@
 /*   By: alvinram <alvinram@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/26 21:44:07 by alvinram          #+#    #+#             */
-/*   Updated: 2025/03/15 22:28:28 by alvinram         ###   ########.fr       */
+/*   Updated: 2025/03/24 23:26:21 by alvinram         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-static void	ft_search_hexadecimal(unsigned int number, const char caseType);
+static void	ft_search_hexadecimal(unsigned int number, int caseType);
 static int	ft_length_hexadecimal(unsigned int number);
 
 int	ft_print_hexadecimal(unsigned int number, const char caseType)
@@ -23,19 +23,22 @@ int	ft_print_hexadecimal(unsigned int number, const char caseType)
 	return (ft_length_hexadecimal(number));
 }
 
-static void	ft_search_hexadecimal(unsigned int number, const char caseType)
+static void	ft_search_hexadecimal(unsigned int number, int caseType)
 {
+	char	hex_char;
+
 	if (number >= 16)
 		ft_search_hexadecimal(number / 16, caseType);
 	if (number % 16 < 10)
-		ft_print_character((number % 16) + '0');
+		hex_char = (number % 16) + '0';
 	else
 	{
-		if (caseType == 'x')
-			ft_print_character((number % 16) - 10 + 'a');
+		if (caseType == 1)
+			hex_char = (number % 16) - 10 + 'A';
 		else
-			ft_print_character((number % 16) - 10 + 'A');
+			hex_char = (number % 16) - 10 + 'a';
 	}
+	ft_print_character(hex_char);
 }
 
 static int	ft_length_hexadecimal(unsigned int number)
