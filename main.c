5f9c0d954538f9f1a08c9b4274e75f3e9e2838e2
Alvin Ramirez <alvin.ramirez48@gmail.com>
2025-03-29 23:37:44 +0100

    test: add main.c to test all printf functionalities

---
+++ main.c
@@ -0,0 +1,56 @@
+#include "ft_printf.h"
+#include <stdio.h>
+
+int main(void)
+{
+    /*
+    char c = 'A';
+    char *str = "Hello, 42!";
+    void *ptr = &str;
+    int num = -12345;
+    unsigned int unum = 12345;
+    int hex_num = 255;
+
+    printf("=== Testing ft_printf ===\n\n");
+
+    // Test %c
+    printf("printf   -> Char: %c\n", c);
+    ft_printf("ft_printf -> Char: %c\n\n", c);
+
+    // Test %s
+    printf("printf   -> String: %s\n", str);
+    ft_printf("ft_printf -> String: %s\n\n", str);
+
+    // Test %p
+    printf("printf   -> Pointer: %p\n", ptr);
+    ft_printf("ft_printf -> Pointer: %p\n\n", ptr);
+
+    // Test %d y %i
+    printf("printf   -> Decimal (%%d): %d\n", num);
+    ft_printf("ft_printf -> Decimal (%%d): %d\n\n", num);
+
+    printf("printf   -> Integer (%%i): %i\n", num);
+    ft_printf("ft_printf -> Integer (%%i): %i\n\n", num);
+
+    // Test %u
+    printf("printf   -> Unsigned (%%u): %u\n", unum);
+    ft_printf("ft_printf -> Unsigned (%%u): %u\n\n", unum);
+
+    // Test %x
+    printf("printf   -> Hexadecimal (%%x): %x\n", hex_num);
+    ft_printf("ft_printf -> Hexadecimal (%%x): %x\n\n", hex_num);
+
+    // Test %X
+    printf("printf   -> Hexadecimal Upper (%%X): %X\n", hex_num);
+    ft_printf("ft_printf -> Hexadecimal Upper (%%X): %X\n\n", hex_num);
+
+    // Test %%
+    printf("printf   -> Percent: %%\n");
+    ft_printf("ft_printf -> Percent: %%\n\n");
+    */
+
+    ft_printf("%X",-10);
+    //ft_printf("%x",-10);
+
+    return (0);
+}
