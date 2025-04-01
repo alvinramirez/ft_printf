aead39ca8265666ff425daeb2d1a2046ba02b573
Alvin Ramirez <alvin.ramirez48@gmail.com>
2025-04-01 23:28:42 +0200

    refactor: optimize functions order

--- ft_printf.c
+++ ft_printf.c
@@ -6,34 +6,15 @@
 /*   By: alvinram <alvinram@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/26 20:48:52 by alvinram          #+#    #+#             */
-/*   Updated: 2025/03/26 22:22:02 by alvinram         ###   ########.fr       */
+/*   Updated: 2025/04/01 15:54:24 by alvinram         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-static int	ft_select_format(va_list args, char specifier)
-{
-	if (specifier == 'c')
-		return (ft_print_character(va_arg(args, int)));
-	if (specifier == 's')
-		return (ft_print_string(va_arg(args, char *)));
-	if (specifier == 'p')
-		return (ft_print_pointer((unsigned long long)va_arg(args, void *)));
-	if (specifier == 'd' || specifier == 'i')
-		return (ft_print_number(va_arg(args, int)));
-	if (specifier == 'u')
-		return (ft_print_unsigned(va_arg(args, unsigned int)));
-	if (specifier == 'x')
-		return (ft_print_hexadecimal(va_arg(args, int), 0));
-	if (specifier == 'X')
-		return (ft_print_hexadecimal(va_arg(args, int), 1));
-	if (specifier == '%')
-		return (ft_safe_write(1, "%", 1));
-	return (-1);
-}
+static int	ft_select_format(va_list args, char specifier);
 
-int	ft_printf(const char *format, ...)
+int	ft_printf(char const *format, ...)
 {
 	va_list	args;
 	int		count;
@@ -54,4 +35,25 @@ int	ft_printf(const char *format, ...)
 	}
 	va_end(args);
 	return (count);
+}
+
+static int	ft_select_format(va_list args, char specifier)
+{
+	if (specifier == 'c')
+		return (ft_print_character(va_arg(args, int)));
+	if (specifier == 's')
+		return (ft_print_string(va_arg(args, char *)));
+	if (specifier == 'p')
+		return (ft_print_pointer((unsigned long long)va_arg(args, void *)));
+	if (specifier == 'd' || specifier == 'i')
+		return (ft_print_number(va_arg(args, int)));
+	if (specifier == 'u')
+		return (ft_print_unsigned(va_arg(args, unsigned int)));
+	if (specifier == 'x')
+		return (ft_print_hexadecimal(va_arg(args, int), 0));
+	if (specifier == 'X')
+		return (ft_print_hexadecimal(va_arg(args, int), 1));
+	if (specifier == '%')
+		return (ft_safe_write(1, "%", 1));
+	return (-1);
 }
