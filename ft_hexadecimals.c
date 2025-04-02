c91889b70d736aaf249986da44aa36dfa640f9d4
Alvin Ramirez <alvin.ramirez48@gmail.com>
2025-04-02 22:46:08 +0200

    fix: correct format specifier handling and remove ft_safe_write usage

--- ft_hexadecimals.c
+++ ft_hexadecimals.c
@@ -6,7 +6,7 @@
 /*   By: alvinram <alvinram@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/26 21:44:07 by alvinram          #+#    #+#             */
-/*   Updated: 2025/04/01 23:26:56 by alvinram         ###   ########.fr       */
+/*   Updated: 2025/04/02 22:08:47 by alvinram         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,11 @@
 
 int	ft_print_hexadecimal(int number, const char caseType)
 {
-	if (number == 0)
+	unsigned int	safe_number;
+
+	safe_number = (unsigned int)number;
+	if (safe_number == 0)
 		return (ft_print_character('0'));
-	ft_search_hexadecimal(number, caseType);
-	return (ft_length_hexadecimal(number));
+	ft_search_hexadecimal(safe_number, caseType);
+	return (ft_length_hexadecimal(safe_number));
 }
--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: alvinram <alvinram@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/26 20:48:52 by alvinram          #+#    #+#             */
-/*   Updated: 2025/04/01 15:54:24 by alvinram         ###   ########.fr       */
+/*   Updated: 2025/04/02 22:25:42 by alvinram         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,7 +29,7 @@ int	ft_printf(char const *format, ...)
 			if (ft_handle_output(&count, ft_select_format(args, *format)) < 0)
 				return (va_end(args), -1);
 		}
-		else if (ft_handle_output(&count, ft_safe_write(1, format, 1)) < 0)
+		else if (ft_handle_output(&count, write(1, format, 1)) < 0)
 			return (va_end(args), -1);
 		format++;
 	}
@@ -50,10 +50,10 @@ static int	ft_select_format(va_list args, char specifier)
 	if (specifier == 'u')
 		return (ft_print_unsigned(va_arg(args, unsigned int)));
 	if (specifier == 'x')
-		return (ft_print_hexadecimal(va_arg(args, int), 0));
+		return (ft_print_hexadecimal(va_arg(args, unsigned int), 'x'));
 	if (specifier == 'X')
-		return (ft_print_hexadecimal(va_arg(args, int), 1));
+		return (ft_print_hexadecimal(va_arg(args, unsigned int), 'X'));
 	if (specifier == '%')
-		return (ft_safe_write(1, "%", 1));
+		return (write(1, "%", 1));
 	return (-1);
 }
