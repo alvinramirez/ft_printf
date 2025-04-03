289e12122e8ae41e73bb94bfdfae2efd13846310
Alvin Ramirez <alvin.ramirez48@gmail.com>
2025-04-03 13:52:18 +0200

    refactor: remove unnecessary pointer validation in ft_search_hexadecimal and add exclusive pointers function for search pointer

--- ft_pointers.c
+++ ft_pointers.c
@@ -6,23 +6,33 @@
 /*   By: alvinram <alvinram@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/26 21:44:11 by alvinram          #+#    #+#             */
-/*   Updated: 2025/04/01 23:26:24 by alvinram         ###   ########.fr       */
+/*   Updated: 2025/04/03 13:31:56 by alvinram         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int	ft_print_pointer(unsigned long long pointer)
+void	ft_search_pointer(unsigned long long number)
 {
-	int	size;
-
-	size = ft_print_string("0x");
-	if (pointer == 0)
-		size += ft_print_character('0');
+	if (number >= 16)
+	{
+		ft_search_pointer(number / 16);
+		ft_search_pointer(number % 16);
+	}
 	else
 	{
-		ft_search_hexadecimal(pointer, 'p');
-		size += ft_length_hexadecimal(pointer);
+		if (number < 10)
+			ft_print_character(number + '0');
+		else
+			ft_print_character(number - 10 + 'a');
 	}
-	return (size);
+}
+
+int	ft_print_pointer(unsigned long long pointer)
+{
+	if (pointer == 0)
+		return (ft_print_string("(nil)"));
+	ft_print_string("0x");
+	ft_search_pointer(pointer);
+	return (ft_length_hexadecimal(pointer) + 2);
 }
--- ft_utils.c
+++ ft_utils.c
@@ -6,7 +6,7 @@
 /*   By: alvinram <alvinram@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/21 16:32:03 by alvinram          #+#    #+#             */
-/*   Updated: 2025/04/02 22:06:06 by alvinram         ###   ########.fr       */
+/*   Updated: 2025/04/03 13:46:07 by alvinram         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,7 +38,7 @@ void	ft_search_hexadecimal(unsigned long long number, char format)
 			ft_print_character(number + '0');
 		else
 		{
-			if (format == 'x' || format == 'p')
+			if (format == 'x')
 				ft_print_character(number - 10 + 'a');
 			else if (format == 'X')
 				ft_print_character(number - 10 + 'A');
